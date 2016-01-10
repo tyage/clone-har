9aba148194a0bff7644a735794b96192117c36bf
tyage <namatyage@gmail.com>
2016-01-10 23:27:55 +0900

    print nginx, /etc/hosts config

--- capture.js
+++ capture.js
@@ -6,17 +6,45 @@ if (process.argv.length < 3) {
   return;
 }
 
-const cwd = 'sites';
+const sitesDir = `${process.cwd()}/sites`;
+const getDomains = () => fs.readdirSync(sitesDir).filter(domain => domain !== '.gitkeep');
 
+// print nginx, etc/hosts config
+const hostsConfig = domain => `127.0.0.1\t${domain}`;
+const nginxConfig = domain => `server {
+  listen 80;
+  server_name ${domain};
+
+  root ${sitesDir}/${domain};
+  index index.html;
+
+  location / {
+    try_files $uri $uri/ =404;
+  }
+}`;
+const printConfig = () => {
+  const domains = getDomains();
+  console.log(`/etc/hosts:
+
+${domains.map(hostsConfig).join('\n')}
+
+nginx settings:
+
+${domains.map(nginxConfig).join('\n')}
+`);
+};
+
+// wget sites
 const file = process.argv[2];
 const data = JSON.parse(fs.readFileSync(file));
 const entries = data.log.entries;
 const next = () => {
   if (entries.length === 0) {
+    printConfig();
     return;
   }
   const entry = entries.pop();
-  const child = child_process.spawn('wget', ['-x', entry.request.url], { cwd });
+  const child = child_process.spawn('wget', ['-x', entry.request.url], { cwd: sitesDir });
   child.on('close', next);
 };
 next();
